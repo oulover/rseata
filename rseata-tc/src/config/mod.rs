a3f1fdf2a84dd988c12d52fd09cebf03bc12d367
openg <595466820@qq.com>
2025-11-09 16:12:32 +0800

    version

--- rseata-tc/src/config/mod.rs
+++ rseata-tc/src/config/mod.rs
@@ -6,7 +6,7 @@ pub fn get_env_web_server_ip() -> String {
     get_env("RSEATA_WEB_SERVER_IP").unwrap_or(String::from("0.0.0.0"))
 }
 pub fn get_env_web_server_prot() -> String {
-    get_env("RSEATA_WEB_SERVER_IP").unwrap_or(String::from("3000"))
+    get_env("RSEATA_WEB_SERVER_PROT").unwrap_or(String::from("3000"))
 }
 pub fn get_env_rseata_traces_exporter() -> String {
     get_env("RSEATA_TRACES_EXPORTER").unwrap_or(String::from("FMT"))
